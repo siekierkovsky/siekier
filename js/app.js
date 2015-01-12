0a679de6d93210e673659cc8d33a9804808e2f79
siekierkovsky <piter_ob@wp.pl>
2015-01-12 19:44:04 +0000

    cokolwiek

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('myApp',[])
+
+    .controller('myCtrl', function($scope, $http){
+        console.log("XXX");
+        var url = 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK';
+        
+        $http.jsonp(url)
+            .success(function(data){
+                $scope.fakeData = data.feed.entry;
+            });
+            
+    });
